e3520e02413017a8062a14ea4701f770754d1aee
Rick Rieger <rickrieger36251@gmail.com>
2021-02-19 13:53:21 -0500

    yay

--- main.js
+++ main.js
@@ -2,6 +2,65 @@
  * YOUR CODE BELOW! *
  ********************/
 
+function moreThan5(num){
+  return answer = num > 5;
+}
+
+function isNewTopScore(score,topScore){
+  return answer = score > topScore;
+}
+
+function isInDanger(grade){
+  return answer = grade >= 60 && grade <= 71;
+}
+
+function isCoasting(grade){
+  return answer = grade >= 72 && grade <= 83;
+}
+
+function isSucceeding(grade){
+  return answer = grade >= 84 && grade <= 92;
+}
+function isFailing(grade){
+  return answer = grade < 60;
+}
+
+function isAcing(grade){
+  return answer = grade > 92;
+}
+
+function isStudent(string){
+  return answer = string === 'student';
+}
+
+function isTeacher(string){
+  return answer = string === 'teacher';
+}
+
+function isAdmin(string){
+  return answer = string === 'admin';
+}
+
+function isElementary(string){
+  return answer = string === 'elementary';
+}
+
+function  areDifferentPeople(name1, name2){
+  return answer = name1 !== name2;
+}
+
+function isMiddleSchoolTeacher(role, level){
+  return answer = role === 'teacher' && level >= 6 && level <= 8;
+}
+
+
+
+function notAnElementarySchoolAdministrator(level, role){
+  return answer = level !== 'elementary' || role !== 'admin';
+}
+
+
+
 
 
 
